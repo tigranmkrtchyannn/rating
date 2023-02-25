3120aa1ce4884978c75f56a40d2730ac47d6983b
tigranmkrtchyannn <mkrtchyantik5@gmail.com>
2023-02-25 13:03:58 +0400

    Add color form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 import colorData from "./color-data.json";
-import ColorList from "./components/ColorList";
+import ColorList from "./components/ColorList.js";
+import AddColorForm from "./components/AddColorForm";
+import { v4 } from "uuid";
 export default function App() {
-  const [colors, setColors] = useState(colorData);
-  return (
-  <ColorList
+const [colors, setColors] = useState(colorData);
+return (
+<>
+<AddColorForm
+onNewColor={(title, color) => {
+const newColors = [
+...colors,
+{
+id: v4(),
+rating: 0,
+title,
+color
+}
+];
+setColors(newColors);
+}}
+/>
+<ColorList
   colors={colors}
   onRateColor={(id, rating) => {
   const newColors = colors.map(color =>
@@ -15,8 +32,7 @@ export default function App() {
   onRemoveColor={id => {
   const newColors = colors.filter(color => color.id !== id);
   setColors(newColors);
-  }}
-  />
-  );
-  }
-  
+  }}/>
+</>
+)
+}
--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,28 +1,28 @@
 
-// import React, { useState } from "react";
-// import { useInput } from "../hook/useInput";
+import React, { useState } from "react";
+import { useInput } from "../hook/useInput";
 
-// export default function AddColorForm({ onNewColor = f => f }) {
-// const [titleProps, resetTitle] = useInput("");
-// const [colorProps, resetColor] = useInput("#000000");
+export default function AddColorForm({ onNewColor = f => f }) {
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
 
-//     const submit = event => {
-// event.preventDefault();
-// onNewColor(titleProps.value, colorProps.value);
-// resetTitle();
-// resetColor();
-// };
+    const submit = event => {
+        event.preventDefault();
+        onNewColor(titleProps.value, colorProps.value);
+        resetTitle();
+        resetColor();
+    };
 
-//     return (
-//         <form onSubmit={submit}>
-//         <input
-//         {...titleProps}
-//         type="text"
-//         placeholder="color title..."
-//         required
-//         />
-//         <input {...colorProps} type="color" required />
-//         <button>ADD</button>
-//         </form>
-//         );
-//     }
+    return (
+        <form onSubmit={submit}>
+            <input
+                {...titleProps}
+                type="text"
+                placeholder="color title..."
+                required
+            />
+            <input {...colorProps} type="color" required />
+            <button>ADD</button>
+        </form>
+    );
+}
